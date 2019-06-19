31e029b073377e537eca7f0f274349c7e0faaaaf
Sosnus <sosnus15@gmail.com>
2019-06-19 12:03:12 +0200

    add npm and sqlite to nodejs config script

--- install-node.sh
+++ install-node.sh
@@ -3,3 +3,15 @@ sudo apt install nodejs
 
 echo "Node install done!"
 
+
+sudo apt install npm
+
+echo "npm install done!"
+
+
+npm install sqlite3
+
+echo "npm install sqlite3 done!"
+
+
+nodejs -v
